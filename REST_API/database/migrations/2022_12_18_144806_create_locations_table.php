caa3d50453ea2d90aa8961c800659b469e52f83a
KacKozak2022 <kk305378@student.polsl.pl>
2022-12-18 21:49:06 +0100

    commit-patch-foreign-key

--- REST_API/database/migrations/2022_12_18_144806_create_locations_table.php
+++ REST_API/database/migrations/2022_12_18_144806_create_locations_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->integer('people_id');
-            $table->foreign('people_id')->references('id')->on('people');
             $table->text('address');
             $table->text('city');
             $table->text('country');
